c4c158861944beecabdf1e2faeb062316ec5ca04
hadleysg <hadleygarff@gmail.com>
2025-03-18 21:42:11 -0600

    Hadley commit past_events

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import PastEvents from "./components/past_events/PastEvents";
+import { Header } from "./components/shared_components/Header";
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <h1>Welcome to the Ministering app!</h1>
-  )
-}
+    <Router>
+      <Header />
+      <Routes>
+        {/* Redirect from root to PastEvents */}
+        <Route path="/" element={<Navigate to="/past-events" />} />
+        <Route path="/past-events" element={<PastEvents />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
