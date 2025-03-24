831388798e7b41936137894b7a9866072837a759
mattej5 <matthew.jones6288@gmail.com>
2025-03-24 12:38:05 -0600

    Inserted people's components

--- src/pages/IdeaGenerator.tsx
+++ src/pages/IdeaGenerator.tsx
@@ -1,7 +1,10 @@
+import IdeaGeneratorPage from "../components/idea_generator/IdeaGeneratorPage";
+
 export default function IdeaGenerator() {
     return (
       <div>
         <h1>Idea Generator</h1>
+        <IdeaGeneratorPage />
       </div>
     );
   }
