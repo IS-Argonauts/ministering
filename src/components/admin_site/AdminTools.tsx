17ffa5d944aea2b55ee40d2490f42b21ff903a0d
Jaden Taylor <jtjadent@gmail.com>
2025-03-25 21:17:13 -0600

    small changes

--- src/components/admin_site/AdminTools.tsx
+++ src/components/admin_site/AdminTools.tsx
@@ -6,12 +6,19 @@ import Jaden from "../../assets/Jaden.jpg"
 function AdminTools() {
   return (
     <>
-      <h2>Hi</h2>
+      <h2>Companionship</h2>
       <div className="grid-container">
         <img src={Bryli} alt="Bryli" className="grid-item" />
         <img src={Dieter} alt="Dieter" className="grid-item" />
         <img src={Jaden} alt="Jaden" className="grid-item" />
       </div>
+      <h2>Assignment</h2>
+      <div className="grid-container">
+        <img src={Bryli} alt="Bryli" className="grid-item" />
+        <img src={Dieter} alt="Dieter" className="grid-item" />
+        <img src={Jaden} alt="Jaden" className="grid-item" />
+      </div>
+
     </>
   );
 }
