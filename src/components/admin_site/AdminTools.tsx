d0994e24592671f367fbef73d8f8b42298bcb412
madierickson <emadisom@byu.edu>
2025-03-26 11:19:20 -0600

    commit

--- src/components/admin_site/AdminTools.tsx
+++ src/components/admin_site/AdminTools.tsx
@@ -37,6 +37,37 @@ function AdminTools() {
           <p className="name">Jaden</p> {/* Name under the image */}
         </div>
       </div>
+      <h2>Companionship</h2>
+      <div className="grid-container">
+        <div className="grid-item">
+          <img src={Bryli} alt="Bryli" className="image" />
+          <p className="name">Bryli</p> {/* Name under the image */}
+        </div>
+        <div className="grid-item">
+          <img src={Dieter} alt="Dieter" className="image" />
+          <p className="name">Dieter</p> {/* Name under the image */}
+        </div>
+        <div className="grid-item">
+          <img src={Jaden} alt="Jaden" className="image" />
+          <p className="name">Jaden</p> {/* Name under the image */}
+        </div>
+      </div>
+
+      <h2>Assignment</h2>
+      <div className="grid-container">
+        <div className="grid-item">
+          <img src={Bryli} alt="Bryli" className="image" />
+          <p className="name">Bryli</p> {/* Name under the image */}
+        </div>
+        <div className="grid-item">
+          <img src={Dieter} alt="Dieter" className="image" />
+          <p className="name">Dieter</p> {/* Name under the image */}
+        </div>
+        <div className="grid-item">
+          <img src={Jaden} alt="Jaden" className="image" />
+          <p className="name">Jaden</p> {/* Name under the image */}
+        </div>
+      </div>
     </>
   );
 }
