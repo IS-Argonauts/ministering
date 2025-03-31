c816a4b1ae3d453ee069d0b734687dc6c072aa10
byubraden <byubraden26@gmail.com>
2025-03-30 19:58:07 -0600

    bradens sync - add user profile view stuff, made some edits to the idea genreator and saved ideas pages

--- src/pages/SavedIdeasPage.tsx
+++ src/pages/SavedIdeasPage.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import SavedIdeas from "../components/idea_generator/SavedIdeas";
 import NavBar from "../NavBar";
 
 export default function SavedIdeasPage() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
   return (
     <div>
-      <NavBar />
+      <NavBar setIsAuthenticated={setIsAuthenticated} />
       <SavedIdeas />
     </div>
   );
