d609270833d9c400ae55fa59fcc305f527df9416
madierickson <emadisom@byu.edu>
2025-03-31 19:48:41 -0600

    test

--- src/components/admin_site/AdminTools.tsx
+++ src/components/admin_site/AdminTools.tsx
@@ -1,73 +1,71 @@
-import React from "react";
 import "./AdminTools.css";
-import Bryli from "../../assets/RandomLady.jpg";
-import Dieter from "../../assets/RandomMan.jpg";
-import Jaden from "../../assets/Jaden.jpg";
+import Larry from "../../images/stockphoto2.jpg";
+import Steve from "../../images/stockPhoto1.jpg";
+import Jaden from "../../images/stockphoto2.jpg";
+import Keith from "../../images/stockphoto2.jpg";
+import Jane from "../../images/stockphoto2.jpg";
+
 function AdminTools() {
   return (
     <>
-      <h2>Companionship</h2>
-      <div className="grid-container">
-        <div className="grid-item">
-          <img src={Bryli} alt="Bryli" className="image" />
-          <p className="name">Bryli</p> {/* Name under the image */}
+      <main>
+        <h1>Admin Tools</h1>
+        <br />
+        <h2>Companionship</h2>
+        <div className="grid-container">
+          <div className="grid-item">
+            <img src={Larry} alt="Larry" className="image" />
+            <p className="name">Larry Smith</p> {/* Name under the image */}
+          </div>
+          <div className="grid-item">
+            <img src={Steve} alt="Steve" className="image" />
+            <p className="name">Steve Boyd</p> {/* Name under the image */}
+          </div>
         </div>
-        <div className="grid-item">
-          <img src={Dieter} alt="Dieter" className="image" />
-          <p className="name">Dieter</p> {/* Name under the image */}
-        </div>
-        <div className="grid-item">
-          <img src={Jaden} alt="Jaden" className="image" />
-          <p className="name">Jaden</p> {/* Name under the image */}
-        </div>
-      </div>
 
-      <h2>Assignment</h2>
-      <div className="grid-container">
-        <div className="grid-item">
-          <img src={Bryli} alt="Bryli" className="image" />
-          <p className="name">Bryli</p> {/* Name under the image */}
+        <h2>Assignment</h2>
+        <div className="grid-container">
+          <div className="grid-item">
+            <img src={Keith} alt="Keith" className="image" />
+            <p className="name">Keith Carney</p> {/* Name under the image */}
+          </div>
+          <div className="grid-item">
+            <img src={Jane} alt="Raymond" className="image" />
+            <p className="name">Jane Ludwig</p> {/* Name under the image */}
+          </div>
         </div>
-        <div className="grid-item">
-          <img src={Dieter} alt="Dieter" className="image" />
-          <p className="name">Dieter</p> {/* Name under the image */}
+        <h2>Companionship</h2>
+        <div className="grid-container">
+          <div className="grid-item">
+            <img src={Keith} alt="Bryli" className="image" />
+            <p className="name">Bryli</p> {/* Name under the image */}
+          </div>
+          <div className="grid-item">
+            <img src={Keith} alt="Dieter" className="image" />
+            <p className="name">Dieter</p> {/* Name under the image */}
+          </div>
+          <div className="grid-item">
+            <img src={Jaden} alt="Jaden" className="image" />
+            <p className="name">Jaden</p> {/* Name under the image */}
+          </div>
         </div>
-        <div className="grid-item">
-          <img src={Jaden} alt="Jaden" className="image" />
-          <p className="name">Jaden</p> {/* Name under the image */}
-        </div>
-      </div>
-      <h2>Companionship</h2>
-      <div className="grid-container">
-        <div className="grid-item">
-          <img src={Bryli} alt="Bryli" className="image" />
-          <p className="name">Bryli</p> {/* Name under the image */}
-        </div>
-        <div className="grid-item">
-          <img src={Dieter} alt="Dieter" className="image" />
-          <p className="name">Dieter</p> {/* Name under the image */}
-        </div>
-        <div className="grid-item">
-          <img src={Jaden} alt="Jaden" className="image" />
-          <p className="name">Jaden</p> {/* Name under the image */}
-        </div>
-      </div>
 
-      <h2>Assignment</h2>
-      <div className="grid-container">
-        <div className="grid-item">
-          <img src={Bryli} alt="Bryli" className="image" />
-          <p className="name">Bryli</p> {/* Name under the image */}
+        <h2>Assignment</h2>
+        <div className="grid-container">
+          <div className="grid-item">
+            <img src={Keith} alt="Bryli" className="image" />
+            <p className="name">Bryli</p> {/* Name under the image */}
+          </div>
+          <div className="grid-item">
+            <img src={Keith} alt="Dieter" className="image" />
+            <p className="name">Dieter</p> {/* Name under the image */}
+          </div>
+          <div className="grid-item">
+            <img src={Jaden} alt="Jaden" className="image" />
+            <p className="name">Jaden</p> {/* Name under the image */}
+          </div>
         </div>
-        <div className="grid-item">
-          <img src={Dieter} alt="Dieter" className="image" />
-          <p className="name">Dieter</p> {/* Name under the image */}
-        </div>
-        <div className="grid-item">
-          <img src={Jaden} alt="Jaden" className="image" />
-          <p className="name">Jaden</p> {/* Name under the image */}
-        </div>
-      </div>
+      </main>
     </>
   );
 }
