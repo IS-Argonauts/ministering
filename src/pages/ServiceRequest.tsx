eb2c014380426e594ab726f6342c23ba42c9f77d
Yuto001 <yuto.t1234@gmail.com>
2025-04-01 16:51:13 -0600

    add padding

--- src/pages/ServiceRequest.tsx
+++ src/pages/ServiceRequest.tsx
@@ -2,7 +2,7 @@ import ServiceRequestPage from "../components/service_request/ServiceRequestPage
 
 export default function Home() {
   return (
-    <div style={{ paddingTop: "90px" }}>
+    <div style={{ paddingTop: "0px" }}>
       <ServiceRequestPage />
     </div>
   );
